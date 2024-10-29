e8b15db2783339d14ba909e6757e0868c0c69662
kien0bede <nthanhkien2k2@gmail.com>
2024-10-29 21:34:27 +0700

    shake-hand-add

--- src/main.cpp
+++ src/main.cpp
@@ -55,13 +55,63 @@ bool heart_sensor_on = true;
 unsigned long lastPressed = 0;
 unsigned long lastWake = 0;
 unsigned long lastDisplayUpdate = 0;
+unsigned long millisHeartRate = 0;
 unsigned long pressStartTime;
 int pressState = 0;
 int Screen = 0;
 int subScreen = 0;
 bool faceChange = false;
 bool wifi_disconnect = true;
+bool screenHeartRate = true;
 
+#define BUTTON_PIN_BITMASK(GPIO) (1ULL << GPIO)  // 2 ^ GPIO_NUMBER in hex
+#define WAKEUP_GPIO_4              GPIO_NUM_4     // Only RTC IO are allowed - ESP32 Pin example
+#define WAKEUP_GPIO_21              GPIO_NUM_21     // Only RTC IO are allowed - ESP32 Pin example
+
+// Define bitmask for multiple GPIOs
+uint64_t bitmask = BUTTON_PIN_BITMASK(WAKEUP_GPIO_4) | BUTTON_PIN_BITMASK(WAKEUP_GPIO_21);
+
+// MPU registers
+#define SIGNAL_PATH_RESET  0x68
+#define ACCEL_CONFIG       0x1C
+#define MOT_THR            0x1F  // Motion detection threshold bits [7:0]
+#define MOT_DUR            0x20  // Duration counter threshold for motion interrupt
+#define MOT_DETECT_CTRL    0x69
+#define INT_ENABLE         0x38
+#define PWR_MGMT           0x6B //SLEEPY TIME
+#define MPU6050_ADDRESS    0x69 //AD0 is 0
+
+void writeByte(uint8_t address, uint8_t subAddress, uint8_t data)
+{
+  Wire.beginTransmission(address);
+  Wire.write(subAddress);
+  Wire.write(data);
+  Wire.endTransmission();
+}
+
+uint8_t readByte(uint8_t address, uint8_t subAddress)
+{
+  uint8_t data;
+  Wire.beginTransmission(address);
+  Wire.write(subAddress);
+  Wire.endTransmission(false);
+  Wire.requestFrom(address, (uint8_t) 1);
+  data = Wire.read();
+  return data;
+}
+
+void configureMPU(int sens){
+  writeByte( MPU6050_ADDRESS, 0x6B, 0x00);
+  writeByte( MPU6050_ADDRESS, SIGNAL_PATH_RESET, 0x07);//Reset all internal signal paths in the MPU-6050 by writing 0x07 to register 0x68;
+  writeByte( MPU6050_ADDRESS, ACCEL_CONFIG, 0x01);//Write register 28 (==0x1C) to set the Digital High Pass Filter, bits 3:0. For example set it to 0x01 for 5Hz. (These 3 bits are grey in the data sheet, but they are used! Leaving them 0 means the filter always outputs 0.)
+  writeByte( MPU6050_ADDRESS, MOT_THR, sens);  //Write the desired Motion threshold to register 0x1F (For example, write decimal 20).
+  writeByte( MPU6050_ADDRESS, MOT_DUR, 40 );  //Set motion detect duration to 1  ms; LSB is 1 ms @ 1 kHz rate
+  writeByte( MPU6050_ADDRESS, MOT_DETECT_CTRL, 0x15); //to register 0x69, write the motion detection decrement and a few other settings (for example write 0x15 to set both free-fall and motion decrements to 1 and accelerometer start-up delay to 5ms total by adding 1ms. )
+  writeByte( MPU6050_ADDRESS, 0x37, 140 ); // now INT pin is active low
+  writeByte( MPU6050_ADDRESS, INT_ENABLE, 0x40 ); //write register 0x38, bit 6 (0x40), to enable motion detection interrupt.
+  writeByte( MPU6050_ADDRESS, PWR_MGMT, 8 ); // 101000 - Cycle & disable TEMP SENSOR
+  writeByte( MPU6050_ADDRESS, 0x6C, 7); // Disable Gyros
+}
 void IRAM_ATTR checkTicks() {
   button.tick();
 }
@@ -72,28 +122,39 @@ void enter_sleep()
   delay(100);
   rtc_gpio_hold_en((gpio_num_t) TFT_BLK_PIN);
   rtc_gpio_hold_en(GPIO_NUM_21);
-  esp_sleep_enable_ext0_wakeup(GPIO_NUM_21, LOW);
+  esp_sleep_enable_ext1_wakeup(bitmask, ESP_EXT1_WAKEUP_ANY_LOW);
   esp_deep_sleep_start();
 }
 
 void ShortClick() {
   printf("singleClick() detected.\n");
-  lastWake = millis();
+  unsigned long currentMillis = millis();
+  lastWake = currentMillis;
+  lastDisplayUpdate = currentMillis;
+  lastPressed = currentMillis;
+
   if (Screen == 0) {
+    if (subScreen == 1 && !screenHeartRate) {
+      millisHeartRate = millis();
+      digitalWrite(TFT_BLK_PIN, HIGH);
+      screenHeartRate = true;
+      return;
+    }
     subScreen++;
+    
     if (subScreen > 3) {
       subScreen = 0;
       faceChange = true;
     }
+
     if (subScreen == 1) {
       particleSensor.wakeUp();
+      millisHeartRate = millis();
     } else {
       particleSensor.shutDown();
     }
   }
   pressState = 1;
-  lastDisplayUpdate = millis();
-  lastPressed = millis();
 }
 
 void LongPress() {
@@ -109,10 +170,11 @@ void LongPress() {
 void setup() {
   Wire.begin(SDA_PIN, SCL_PIN);
   // Init gia toc
-  mpu.begin(0x69);
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  // mpu.begin(0x69);
+  // mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  // mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  // mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  configureMPU(5);
   // Init la ban
   compass.init();
   // Init nhip tim
@@ -193,6 +255,11 @@ void watchFace() {
 
 void heartRateApp() {
   long irValue = particleSensor.getIR();
+  if (millis() - millisHeartRate > 10000)
+  {
+    digitalWrite(TFT_BLK_PIN, LOW);
+    screenHeartRate = false;
+  }
 
   if (checkForBeat(irValue) == true)
   {
@@ -240,7 +307,7 @@ void watchtask() {
   if (pressState == 1 && digitalRead(0) == 1) {
     pressState = 0;
   }
-  if (millis() - lastWake > 30000) {
+  if (millis() - lastWake > 10000) {
     enter_sleep();
   }
   if (Screen == 0) {
